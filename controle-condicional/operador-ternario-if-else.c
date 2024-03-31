4ac257d99b0f8dd9af2c2ddf99ea9256015833ad
Nick <160781135+Nicolau-369@users.noreply.github.com>
2024-03-31 12:51:09 -0300

    Update operador-ternario-if-else.c

---
+++ controle-condicional/operador-ternario-if-else.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+	int x,y,z;
+	printf(“Digite x:”);
+	scanf(“%d”,&x);
+	printf(“Digite y:”);
+	scanf(“%d”,&y);
+	if(x > y)
+		z = x;
+	else
+		z = y;
+	printf(“Maior = %d\n”,z);
+	system(“pause”);
+	return 0;
+}
