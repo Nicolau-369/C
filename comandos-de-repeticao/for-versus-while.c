98336d7e48c935d346fb310c64dd1c29f32349bc
Nick <160781135+Nicolau-369@users.noreply.github.com>
2024-04-01 15:54:30 -0300

    Update for-versus-while.c

---
+++ comandos-de-repeticao/for-versus-while.c
@@ -0,0 +1,27 @@
+// For
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    int i, s = 0;
+    for(i = 1; i <= 10; i++){
+        s = s + i;
+    }
+    printf(“Soma = %d \n”,s);
+    system(“pause”);
+    return 0;
+}
+
+// While
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    int i, s = 0;
+    i = 1
+    while (i <= 10){
+        s = s + i;
+        i++;
+    }
+    printf(“Soma = %d \n”,s);
+    system(“pause”);
+    return 0;
+}
