cc48f5db8da58b7b8e5bf5f3e717e047e94214b9
Nick <160781135+Nicolau-369@users.noreply.github.com>
2024-04-01 15:52:00 -0300

    Update loop-infinito.c

---
+++ comandos-de-repeticao/loop-infinito.c
@@ -0,0 +1,12 @@
+// Erro
+X recebe 4;
+enquanto (X < 5) faca
+    X recebe X – 1;
+    Imprima X;
+fim enquanto
+
+// não muda valor
+X recebe 4;
+enquanto (X < 5) faca
+    Imprima X;
+fim enquanto
