e026afc0aaaaf3a99405c7042702b6cc73c5062b
Nick <160781135+Nicolau-369@users.noreply.github.com>
2024-04-03 10:03:01 -0300

    Add files via upload

---
+++ inicializacao-sem-tamanho.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    // Declara um array com 10 posições
+    int vetor[] = {1,2,3,4,5,6,7,8,9,10};
+
+    system(“pause”);
+    return 0;
+}
+
+// string
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    //A string texto terá tamanho 13
+    //(12 caracteres + o caractere ‘\0’)
+    char texto[] = “Linguagem C.”;
+
+    system(“pause”);
+    return 0;
+}
+
+// matriz
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    //O número de linhas de matriz será 5.
+    int matriz[][2] = {1,2,3,4,5,6,7,8,9,10};
+
+    system(“pause”);
+    return 0;
+}
