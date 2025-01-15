12f79133da078c141843bd5b5b46ab62f7207f8e
Nick <160781135+Nicolau-369@users.noreply.github.com>
2025-01-15 17:41:15 -0300

    Update and rename arrays-de-uma-dimensao.c to one-dimensional-arrays.c

--- alocacao/one-dimensional-arrays.c
+++ alocacao/one-dimensional-arrays.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(){
-    int **p; //2 “*” = 2 níveis = 2 dimensões
+    int **p; //2 "*" = 2 levels = 2 dimensions
     int i, j, N = 2;
     p = (int **) malloc(N*sizeof(int *));
     for (i = 0; i < N; i++){
         p[i] = (int *) malloc(N*sizeof(int));
         for (j = 0; j < N; j++)
-            scanf(“%d”,&p[i][j]);
+            scanf("%d",&p[i][j]);
     }
     for (i = 0; i < N; i++){
         free(p[i]);
     }
     free(p);
-    system(“pause”);
+    system("pause");
     return 0;
 }
