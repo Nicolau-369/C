7cbe4d19f0efa1f42be14d21e0bcc2922b511513
Nick <160781135+Nicolau-369@users.noreply.github.com>
2025-01-15 18:55:02 -0300

    Update fputc()-fputs().c

--- arquivos/fputc()-fputs().c
+++ arquivos/fputc()-fputs().c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(){
-    char texto[30] = “Hello World\n”;
-    fputs(texto, stdout);
-    system(“pause”);
+    char text[30] = "Hello World\n";
+    fputs(text, stdout);
+    system("pause");
     return 0;
 }
