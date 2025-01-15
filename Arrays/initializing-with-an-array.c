aefda17b0a0e0370fdeb032332cefc3716427750
Nick <160781135+Nicolau-369@users.noreply.github.com>
2025-01-15 15:16:20 -0300

    Update and rename inicializando-com-uma-matriz.c to initializing-with-an-array.c

---
+++ Arrays/initializing-with-an-array.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+
+    float vetor[3] = {1.5,22.1,4.56};
+    int headquarters1[3][4] = {1,2,3,4,5,6,7,8,9,10,11,12};
+    int headquarters2[3][4] = {{1,2,3,4}, {5,6,7,8}, {9,10,11,12}};
+
+    system("pause");
+    return 0;
+}
