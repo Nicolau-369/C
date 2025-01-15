0c975cdae04c5ea731afffe1d113cb79741ec4a3
Nick <160781135+Nicolau-369@users.noreply.github.com>
2025-01-15 19:57:34 -0300

    Update fwrite()+count.c

--- arquivos/fwrite()+count.c
+++ arquivos/fwrite()+count.c
@@ -2,21 +2,21 @@
 #include <stdlib.h>
 int main(){
     FILE *arq;
-    arq = fopen(“ArqGrav.txt”,“wb”);
+    arq = fopen("ArqGrav.txt","wb");
     if(arq == NULL){
-        printf(“Problemas na CRIACAO do arquivo\n”);
-        system(“pause”);
+        printf("Problems creating the file\n");
+        system("pause");
         exit(1);
     }
-    int total _ gravado, v[5] = {1,2,3,4,5};
-    //grava todo o array no arquivo (5 posições)
-    total _ gravado = fwrite(v,sizeof(int),5,arq);
-    if(total _ gravado != 5){
-        printf(“Erro na escrita do arquivo!”);
-        system(“pause”);
+    int total _ recorded, v[5] = {1,2,3,4,5};
+    // writes the entire array to the file (5 positions)
+    total _ recorded = fwrite(v,sizeof(int),5,arq);
+    if(total _ recorded != 5){
+        printf("Error writing file!");
+        system("pause");
         exit(1);
     }
     fclose(arq);
-    system(“pause”);
+    system("pause");
     return 0;
 }
