d5f1bc3e085acd379b32137f03b69e258d02a5e4
Nick <160781135+Nicolau-369@users.noreply.github.com>
2025-01-15 19:01:35 -0300

    Update fputc().c

--- arquivos/fputc().c
+++ arquivos/fputc().c
@@ -5,18 +5,18 @@ int main(){
     FILE *arq;
     char string[100];
     int i;
-    arq = fopen(“arquivo.txt”,“w”);
+    arq = fopen("archive.txt","w");
     if(arq == NULL){
-        printf(“Erro na abertura do arquivo”);
-        system(“pause”);
+        printf("Error opening file");
+        system("pause");
         exit(1);
     }
-    printf(“Entre com a string a ser gravada no arquivo:”);
+    printf("Enter the string to be written to the file:");
     gets(string);
-    //Grava a string, caractere a caractere
+    // Write the string, character by character
     for(i = 0; i < strlen(string); i++)
         fputc(string[i], arq);
     fclose(arq);
-    system(“pause”);
+    system("pause");
     return 0;
 }
