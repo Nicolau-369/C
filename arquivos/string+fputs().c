6b5a5554268688c6159e6ab6403a42c58f8b3c41
Nick <160781135+Nicolau-369@users.noreply.github.com>
2025-01-15 20:12:54 -0300

    Update string+fputs().c

--- arquivos/string+fputs().c
+++ arquivos/string+fputs().c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(){
-    char str[20] = “Hello World!”;
+    char str[20] = "Hello World!";
     int result;
     FILE *arq;
-    arq = fopen(“ArqGrav.txt”,“w”);
+    arq = fopen("ArqGrav.txt","w");
     if(arq == NULL) {
-        printf(“Problemas na CRIACAO do arquivo\n”);
-        system(“pause”);
+        printf("Problems CREATING the file\n");
+        system("pause");
         exit(1);
     }
     result = fputs(str,arq);
     if(result == EOF)
-        printf(“Erro na Gravacao\n”);
+        printf("Recording Error\n");
 
     fclose(arq);
-    system(“pause”);
+    system("pause");
     return 0;
 }
