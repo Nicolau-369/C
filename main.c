ffef11197170b9716f474fca5d0408ced3258afc
Nick <160781135+Nicolau-369@users.noreply.github.com>
2025-01-15 13:33:20 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
-int fatorial(int n) {
+
+int factorial(int n){
     int i,f = 1;
-    for (i = 1; i <= n; i++)
+    for(i = 1; i <= n; i++)
         f = f * i;
-    return f;
+    return f;    
 }
+
 int main(){
     int x,y;
-    printf("Digite um valor inteiro: ");
-    scanf("%d",&x);
-    if(x > 0) {
-        printf("X eh positivo\n");
-        y = fatorial(x);
-        printf("Fatorial de X eh %d\n",y);
-    }else {
+    printf("Enter an integer value: ");
+    scanf("%d", &x);
+    if(x > 0){
+        printf("X is positive\n");
+        y = factorial(x);
+        printf("Factorial of X is %d\n",y);
+    }else{
         if(x < 0)
-            printf("X eh negativo\n");
+            printf("X is negative\n");
+        else
+            printf("X is Zero\n");    
     }
-    printf("Fim do programa\n");
+    printf("End of program!\n");
     system('pause');
     return 0;
 }
