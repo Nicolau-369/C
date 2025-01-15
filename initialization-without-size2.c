171483d172b0e2bae76e104ca7a4d1c0e5a3e197
Nick <160781135+Nicolau-369@users.noreply.github.com>
2025-01-15 14:01:00 -0300

    Add files via upload

---
+++ initialization-without-size2.c
@@ -0,0 +1,11 @@
+// string
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    // The text string will be 13 in length
+    // (12 characters + the ‘\0’ character)
+    char text[] = "C-Language.";
+
+    system('pause');
+    return 0;
+}
---
+++ initialization-without-size3.c
@@ -0,0 +1,10 @@
+// headquarters
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    // The number of matrix rows will be 5.
+    int headquarters[][2] = {1,2,3,4,5,6,7,8,9,10};
+
+    system('pause');
+    return 0;
+}
