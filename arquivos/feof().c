72d5a2b9e257880dc47d3f8c777e806349af525c
Nick <160781135+Nicolau-369@users.noreply.github.com>
2025-01-15 18:51:10 -0300

    Update feof().c

--- arquivos/feof().c
+++ arquivos/feof().c
@@ -3,17 +3,17 @@
 int main(){
     FILE *fp;
     char c;
-    fp = fopen(“arquivo.txt”,“r”);
+    fp = fopen("arquivo.txt","r");
     if(fp==NULL){
-        printf(“Erro na abertura do arquivo\n”);
-        system(“pause”);
+        printf("Error opening file\n");
+        system("pause");
         exit(1);
     }
     while(!feof(fp)){
         c = fgetc(fp);
-        printf(“%c”,c);
+        printf("%c",c);
     }
     fclose(fp);
-    system(“pause”);
+    system("pause");
     return 0;
 }
