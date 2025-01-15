3fe3cfcb80085ab0c7882ea121c3917051a09f64
Nick <160781135+Nicolau-369@users.noreply.github.com>
2025-01-15 14:27:53 -0300

    Update array-com-vetor.c

--- Arrays/array-com-vetor.c
+++ Arrays/array-com-vetor.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(){
-    int notas[100];
+    int notes[100];
     int i;
     for (i = 0; i < 100; i++){
-        printf(“Digite a nota do aluno %d”,i);
-        scanf(“%d”,&notas[i]);
+        printf("Enter the students grade %d",i);
+        scanf("%d",&notes[i]);
     }
-    system(“pause”);
+    system('pause');
     return 0;
 }
