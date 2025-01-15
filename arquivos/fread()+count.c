bbaba8d889815ec0cb9150b655e1add685fd8c39
Nick <160781135+Nicolau-369@users.noreply.github.com>
2025-01-15 19:15:13 -0300

    Update fread()+count.c

--- arquivos/fread()+count.c
+++ arquivos/fread()+count.c
@@ -2,24 +2,24 @@
 #include <stdlib.h>
 int main(){
     FILE *arq;
-    arq = fopen(“ArqGrav.txt”,“rb”);
+    arq = fopen("ArqGrav.txt","rb");
     if(arq == NULL){
-        printf(“Problemas na ABERTURA do arquivo\n”);
-        system(“pause”);
+        printf("Problems OPENING the file\n");
+        system("pause");
         exit(1);
     }
-    int i,total _ lido, v[5];
-    //lê 5 posições inteiras do arquivos
-    total _ lido = fread(v,sizeof(int),5,arq);
-    if(total _ lido != 5){
-        printf(“Erro na leitura do arquivo!”);
-        system(“pause”);
+    int i,total _ read, v[5];
+    // reads 5 integer positions from the files
+    total _ read = fread(v,sizeof(int),5,arq);
+    if(total _ read != 5){
+        printf("Error reading file!");
+        system("pause");
         exit(1);
     }else{
         for(i = 0; i < 5; i++)
-            printf(“v[%d] = %d\n”,i,v[i]);
+            printf("v[%d] = %d\n",i,v[i]);
     }
     fclose(arq);
-    system(“pause”);
+    system("pause");
     return 0;
 }
