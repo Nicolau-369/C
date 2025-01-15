fe7df59e50a3488305e483087b36191aa060b2a8
Nick <160781135+Nicolau-369@users.noreply.github.com>
2025-01-15 20:04:39 -0300

    Update and rename gravando-matriz.c to recording-matrix.c

--- arquivos/recording-matrix.c
+++ arquivos/recording-matrix.c
@@ -2,18 +2,18 @@
 #include <stdlib.h>
 int main(){
     FILE *arq;
-    arq = fopen(“matriz.txt”,“w”);
+    arq = fopen("matrix.txt","w");
     if(arq == NULL){
-        printf(“Erro\n”);
-        system(“pause”);
+        printf("Error\n");
+        system("pause");
         exit(1);
     }
     int mat[2][2] = {{1,2},{3,4}};
     int i,j;
     for(i = 0; i < 2; i++)
         for(j = 0; j < 2; j++)
-            fprintf(arq,“%d\n”,mat[i][j]);
+            fprintf(arq,"%d\n",mat[i][j]);
     fclose(arq);
-    system(“pause”);
+    system("pause");
     return 0;
 }
