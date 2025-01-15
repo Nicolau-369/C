4923d95d790cd9c3c428ac22d5cba5693e03c070
Nick <160781135+Nicolau-369@users.noreply.github.com>
2025-01-15 14:11:09 -0300

    Update array-to-zero.c

--- array-to-zero.c
+++ array-to-zero.c
@@ -4,10 +4,10 @@ int main(){
     int i;
     float v1[10] = {1,2,3};
     int v2[10] = {11,22,33};
-    char str[10] = “nick”;
+    char str[10] = "nick";
     for (i=0; i< 10; i++)
-        printf(“%f %d %c\n”,v1[i], v2[i], str[i]);
+        printf("%f %d %c\n",v1[i], v2[i], str[i]);
 
-    system(“pause”);
+    system("pause");
     return 0;
 }
