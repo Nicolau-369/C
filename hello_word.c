158439c6656ed5169c975e007acd872d0e890455
Nick <160781135+Nicolau-369@users.noreply.github.com>
2025-01-15 14:13:45 -0300

    Add files via upload

---
+++ hello_word.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+    /*
+     The printf() function
+     is used to
+     write to the screen
+   */
+    printf("Hello word \n");
+    // pause the program
+    system("pause");
+    return 0;
+}
