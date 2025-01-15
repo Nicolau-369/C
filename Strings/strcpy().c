cdc87f61adbd070871c84922cebe02976c8a7531
Nick <160781135+Nicolau-369@users.noreply.github.com>
2025-01-15 17:14:31 -0300

    Update strcpy().c

--- Strings/strcpy().c
+++ Strings/strcpy().c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 int main(){
     char str1[100], str2[100];
-    printf(“Entre com uma string: ”);
+    printf("Enter a string: ");
     gets(str1);
     strcpy(str2, str1);
-    printf(“String 1: %s\n”,str1);
-    printf(“String 2: %s\n”,str2);
-    system(“pause”);
+    printf("String 1: %s\n",str1);
+    printf("String 2: %s\n",str2);
+    system("pause");
     return 0;
 }
