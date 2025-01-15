92328f5965472e8369e11ec61422059a56676563
Nick <160781135+Nicolau-369@users.noreply.github.com>
2025-01-15 17:29:06 -0300

    Update and rename alocacao-de-ponteiro-para-ponteiro.c to pointer-to-pointer-allocation.c

--- alocacao/pointer-to-pointer-allocation.c
+++ alocacao/pointer-to-pointer-allocation.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(){
-    int **p; //2 “*” = 2 níveis = 2 dimensões
+    int **p; //2 "*" = 2 levels = 2 dimensions
     int i, j, N = 3;
     p = (int **) malloc(N*sizeof(int *));
     for (i = 0; i < N; i++){
         p[i] = (int *) malloc((i+1)*sizeof(int));
         for (j = 0; j < (i+1); j++)
-            scanf(“%d”,&p[i][j]);
+            scanf("%d",&p[i][j]);
     }
     for (i = 0; i < N; i++){
         free(p[i]);
     }
     free(p);
-    system(“pause”);
+    system("pause");
     return 0;
 }
