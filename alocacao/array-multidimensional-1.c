197d9ccc20b3b3fd7762fd7d4d9aa7655014c48d
Nick <160781135+Nicolau-369@users.noreply.github.com>
2025-01-15 17:34:50 -0300

    Update array-multidimensional-1.c

--- alocacao/array-multidimensional-1.c
+++ alocacao/array-multidimensional-1.c
@@ -2,15 +2,15 @@
 #include <stdlib.h>
 int main(){
     int *p;
-    int i, j, Nlinhas = 2, Ncolunas = 2;
-    p = (int *) malloc(Nlinhas * Ncolunas * sizeof(int));
-    for (i = 0; i < Nlinhas; i++){
-        for (j = 0; j < Ncolunas; j++){
-            p[i * Ncolunas + j] = i+j;//CORRETO
-            p[i][j] = i+j;//ERRADO
+    int i, j, Nlines = 2, Ncolumns = 2;
+    p = (int *) malloc(Nlines * Ncolumns * sizeof(int));
+    for (i = 0; i < Nlines; i++){
+        for (j = 0; j < Ncolumns; j++){
+            p[i * Ncolumns + j] = i+j;//CORRECT
+            p[i][j] = i+j;//WRONG
         }
     }
     free(p);
-    system(“pause”);
+    system("pause");
     return 0;
 }
