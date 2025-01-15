66a4f23904fdfe9b5ee6f0e381f46b9ebb8d65b2
Nick <160781135+Nicolau-369@users.noreply.github.com>
2025-01-15 17:09:45 -0300

    Update strcat().c

--- Strings/strcat().c
+++ Strings/strcat().c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(){
-    char str1[15] = “bom ”;
-    char str2[15] = “dia”;
+    char str1[15] = 'Good';
+    char str2[15] = 'Morning';
     strcat(str1,str2);
-    printf(“%s”,str1);
-    system(“pause”);
+    printf('%s',str1);
+    system('pause');
     return 0;
 }
