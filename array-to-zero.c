2120dd08f3170192d23c3313dca3ecb44a7444ad
Nick <160781135+Nicolau-369@users.noreply.github.com>
2025-01-15 14:06:36 -0300

    Update and rename array-ao-zero.c to array-to-zero.c

--- array-to-zero.c
+++ array-to-zero.c
@@ -4,7 +4,7 @@ int main(){
     int i;
     float v1[10] = {1,2,3};
     int v2[10] = {11,22,33};
-    char str[10] = “joao”;
+    char str[10] = “nick”;
     for (i=0; i< 10; i++)
         printf(“%f %d %c\n”,v1[i], v2[i], str[i]);
 
