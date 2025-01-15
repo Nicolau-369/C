a0a57193bb9387787d3c40fd78469c648b6e662f
Nick <160781135+Nicolau-369@users.noreply.github.com>
2025-01-15 14:54:24 -0300

    Update and rename dimensao-1.c to dimension_1.c

---
+++ Arrays/dimension_1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    // declare array of int with 1 dimension
+    int vet[5];
+    // declare float array with 2 dimensions
+    float mat[5][5];
+    // declare array of double with 3 dimensions
+    double cub[5][5][5];
+    // declare array of int with 4 dimensions
+    int X[5][5][5][5];
+    system("pause");
+    return 0;
+}
