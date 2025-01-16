842939899bc295e65fcbd2d21bcd49d7889803d6
Nick <160781135+Nicolau-369@users.noreply.github.com>
2025-01-15 23:22:07 -0300

    Update variaveis-1.c

--- variaveis/variaveis-1.c
+++ variaveis/variaveis-1.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(){
-    int a = 125;//valor decimal
-    int b = 0435;//valor em octal, equivale a 285
-    int c = 0xFA;//valor em hexadecima, equivale a 506
-    printf("Valor de a: %d\n",a);
-    printf("Valor de b: %d\n",b);
-    printf("Valor de c: %d\n",c);
+    int a = 125;//decimal value
+    int b = 0435;//value in octal, equivalent to 285
+    int c = 0xFA;//value in hexadecimal, equivalent to 506
+    printf("Value of a: %d\n",a);
+    printf("Value of b: %d\n",b);
+    printf("Value of c: %d\n",c);
     system("pause");
     return 0;
 }
