1b1bdc8854b4c60f2b2a9bd7fb390c0e48f69536
Nick <160781135+Nicolau-369@users.noreply.github.com>
2025-01-15 22:15:26 -0300

    Update and rename operador-limitado.c to limited-operator.c

---
+++ controle-condicional/limited-operator.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+	int num;
+	printf("Enter a number: ");
+	scanf("%d", &num);
+	(num == 10)? printf("The number is equal to 10.\n") : printf("("The number
+ 		and different from 10.\n");
+	system("pause");
+	return 0;
+}
