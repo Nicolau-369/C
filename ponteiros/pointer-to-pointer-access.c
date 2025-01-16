e1c25df77075cd509cf458609263fe58f6c636d5
Nick <160781135+Nicolau-369@users.noreply.github.com>
2025-01-16 00:20:04 -0300

    Update pointer-to-pointer-access.c

--- ponteiros/pointer-to-pointer-access.c
+++ ponteiros/pointer-to-pointer-access.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(){
-    char letter=‘a’;
+    char letter='a';
     char *ptrChar = &letter;
     char **ptrPtrChar = &ptrChar;
     char ***ptrPtr = &ptrPtrChar;
-    printf(“Content in *ptrChar: %c\n”,*ptrChar);
-    printf(“Content in **ptrPtrChar:
-    %c\n”,**ptrPtrChar);
-    printf(“Content in ***ptrPtr: %c\n”,***ptrPtr);
-    system(“pause”);
+    printf("Content in *ptrChar: %c\n",*ptrChar);
+    printf("Content in **ptrPtrChar:
+    %c\n",**ptrPtrChar);
+    printf("Content in ***ptrPtr: %c\n",***ptrPtr);
+    system("pause");
     return 0;
 }
