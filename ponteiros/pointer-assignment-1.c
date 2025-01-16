d21de7d79d361d06d3e3f685067b32a38d7e91d3
Nick <160781135+Nicolau-369@users.noreply.github.com>
2025-01-16 00:16:35 -0300

    Update and rename atribuicao-de-ponteiros-1.c to pointer-assignment-1.c

---
+++ ponteiros/pointer-assignment-1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    //Hexadecimal address of serial port
+    int *p = 0x3F8;
+    //The value in decimal is converted to its value
+    haxadecimal: 0x5DC
+    int *p1 = 1500;
+    printf("Address in p: %p \n",p);
+    printf("Address in p1: %p \n",p1);
+    system("pause");
+    return 0;
+}
