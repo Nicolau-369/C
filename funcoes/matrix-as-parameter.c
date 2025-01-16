fae8caeee3c9891b309838338f8fdf5e53582b7e
Nick <160781135+Nicolau-369@users.noreply.github.com>
2025-01-16 11:52:07 -0300

    Update and rename matriz-como-parametro.c to matrix-as-parameter.c

--- funcoes/matrix-as-parameter.c
+++ funcoes/matrix-as-parameter.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void imprime _ matriz(int m[][2], int n){
+void print _ matrix(int m[][2], int n){
     int i,j;
     for (i=0; i<n;i++)
         for (j=0; j< 2;j++)
-            printf(“%d \n”, m[i][j]);
+            printf("%d \n", m[i][j]);
 }
 
 int main(){
     int mat[3][2] = {{1,2},{3,4},{5,6}};
-    imprime _ matriz(mat,3);
-    system(“pause”);
+    print _ matrix(mat,3);
+    system("pause");
     return 0;
 }
