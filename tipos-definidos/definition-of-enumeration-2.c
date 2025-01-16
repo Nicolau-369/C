0d0e15919ffaf830211701cf3bed3f2b045e256e
Nick <160781135+Nicolau-369@users.noreply.github.com>
2025-01-16 16:40:18 -0300

    Update and rename definicao-da-enumeracao-2.c to definition-of-enumeration-2.c

---
+++ tipos-definidos/definition-of-enumeration-2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+enum escapes {setback='\b', tab='\t', newline='\n'};
+int main(){
+    enum escapes and = newline;
+    printf("Test %c of %c written\n",e,e);
+    e = tab;
+    printf("Test %c of %c written\n",e,e);
+    system("pause");
+    return 0;
+}
