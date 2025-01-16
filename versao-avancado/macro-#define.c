a9a327a602f8af42b889997a1f22605ce4547e12
Nick <160781135+Nicolau-369@users.noreply.github.com>
2025-01-16 13:44:42 -0300

    Update macro-#define.c

--- versao-avancado/macro-#define.c
+++ versao-avancado/macro-#define.c
@@ -1,17 +1,17 @@
-// Com macro
+// With macro
 #include <stdio.h>
 #include <stdlib.h>
-#define maior(x,y) x>y?x:y
+#define bigger(x,y) x>y?x:y
 int main(){
     int a = 5;
     int b = 8;
     int c = maior(a,b);
-    printf(“Maior valor = %d\n”,c);
-    system(“pause”);
+    printf("Highest value = %d\n",c);
+    system("pause");
     return 0;
 }
 
-// Sem macro
+// No macro
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -19,12 +19,12 @@ int main(){
     int a = 5;
     int b = 8;
     int c = a>b?a:b;
-    printf(“Maior valor = %d\n”,c);
-    system(“pause”);
+    printf("Highest value = %d\n",c);
+    system("pause");
     return 0;
 }
 
-// macros com parênteses
+// macros with parentheses
 #include <stdio.h>
 #include <stdlib.h>
 #define prod1(x,y) x*y;
@@ -33,22 +33,22 @@ int main(){
     int a = 1, b = 2;
     int c = prod1(a+2,b);
     int d = prod2(a+2,b);
-    printf(“Valor de c = %d\n”,c);
-    printf(“Valor de d = %d\n”,d);
-    system(“pause”);
+    printf("Highest value c = %d\n",c);
+    printf("Highest value d = %d\n",d);
+    system("pause");
     return 0;
 }
 
-// criando uma macro com #define e chaves ({ })
+// creating a macro with #define and curly braces ({ })
 #include<stdio.h>
 #include<stdlib.h>
-#define TROCA(a,b,c) {c t=a; a=b; b=t;}
+#define REPLACEMENT(a,b,c) {c t=a; a=b; b=t;}
 int main(){
     int x=10;
     int y=20;
-    printf(“%d %d\n”, x, y);
-    TROCA(x, y, int);
-    printf(“%d %d\n”, x, y);
-    system(“pause”);
+    printf("%d %d\n", x, y);
+    REPLACEMENT(x, y, int);
+    printf("%d %d\n", x, y);
+    system("pause");
     return 0;
 }
