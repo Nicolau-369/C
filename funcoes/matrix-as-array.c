ba19be86f122e0056c2d9739b4955281cec32b6c
Nick <160781135+Nicolau-369@users.noreply.github.com>
2025-01-16 11:50:34 -0300

    Update and rename matriz-como-array.c to matrix-as-array.c

--- funcoes/matrix-as-array.c
+++ funcoes/matrix-as-array.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void imprime _ matriz(int *m, int n){
+void print _ matrix(int *m, int n){
     int i;
     for (i=0; i<n;i++)
-        printf(“%d \n”, m[i]);
+        printf("%d \n", m[i]);
 }
 
 int main(){
     int mat[3][2] = {{1,2},{3,4},{5,6}};
-    imprime _ matriz(&mat[0][0],6);
-    system(“pause”);
+    print _ matrix(&mat[0][0],6);
+    system("pause");
     return 0;
 }
