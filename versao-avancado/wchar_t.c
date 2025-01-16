8271f683872b763f72add434f72b4517c86f43eb
Nick <160781135+Nicolau-369@users.noreply.github.com>
2025-01-16 14:34:04 -0300

    Update wchar_t.c

--- versao-avancado/wchar_t.c
+++ versao-avancado/wchar_t.c
@@ -2,12 +2,12 @@
 #include <locale.h>
 #include <wchar.h>
 int main(){
-    setlocale(LC _ ALL,“”);
-    wchar _ t caractere = L’\u00A9’;
-    wchar _ t texto[30] = L“meu caractere longo = \u00F1”;
-    wprintf(L“Marca registrada: \u00A9 ou \u00AE \n”);
-    wprintf(L“Caractere: %lc \n”,caractere);
-    wprintf(L“%ls \n”,texto);
-    system(“pause”);
+    setlocale(LC _ ALL,"");
+    wchar _ t character = L'\u00A9';
+    wchar _ t text[30] = L"my long character = \u00F1";
+    wprintf(L"Marca registrada: \u00A9 or \u00AE \n");
+    wprintf(L"Character: %lc \n",caractere);
+    wprintf(L"%ls \n",text);
+    system("pause");
     return 0;
 }
