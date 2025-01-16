6cbe5957600e5e8e3cbe44d6bb828518bd653ec2
Nick <160781135+Nicolau-369@users.noreply.github.com>
2025-01-16 13:37:52 -0300

    Update and rename justificando-valor.c to justifying-value.c

---
+++ versao-avancado/justifying-value.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    int n = 5;
+    //justifies the right
+    printf("n = %5d\n",n);
+    //justifies the left
+    printf("n = %-5d\n",n);
+    system("pause");
+    return 0;
+}
