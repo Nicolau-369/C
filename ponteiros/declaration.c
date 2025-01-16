b216df01990a08f60cd36554ae9e8aabc7597eae
Nick <160781135+Nicolau-369@users.noreply.github.com>
2025-01-16 00:27:22 -0300

    Update and rename declaracao.c to declaration.c

---
+++ ponteiros/declaration.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    //Declares a pointer to int
+    int *p;
+    //Declares a pointer to int
+    float *x;
+    //Declares a pointer to char
+    char *y;
+    //Declares a variable of type int and a pointer to int
+    int soma, *p2,;
+    system("pause");
+    return 0;
+}
