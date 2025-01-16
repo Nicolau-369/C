59df06a291030f775019065a2b9104f40c16da77
Nick <160781135+Nicolau-369@users.noreply.github.com>
2025-01-16 15:03:17 -0300

    Update and rename sequencia-escape.c to escape-sequence.c

---
+++ versao-avancado/escape-sequence.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    //Hexadecimal value character
+    printf ("Character 3D: \x3D \n");
+    printf ("Character 5A: \x5A \n");
+    //Octal value character
+    printf ("Character 44: \044 \n");
+    printf ("Character 105: \105 \n");
+    system("pause");
+    return 0;
+}
