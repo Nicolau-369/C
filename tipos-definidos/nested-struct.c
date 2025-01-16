79c6668481c76234fd5c24d3120f2b9db0c9b201
Nick <160781135+Nicolau-369@users.noreply.github.com>
2025-01-16 16:50:12 -0300

    Update and rename struct-aninhada.c to nested-struct.c

---
+++ tipos-definidos/nested-struct.c
@@ -0,0 +1,9 @@
+struct address{
+    char street[50];
+    int number;
+};
+struct registration{
+    char name[50];
+    int age;
+struct address ender;
+};
--- tipos-definidos/struct-aninhada.c
+++
@@ -1,9 +0,0 @@
-struct endereco{
-    char rua[50];
-    int numero;
-};
-struct cadastro{
-    char nome[50];
-    int idade;
-    struct endereco ender;
-};
