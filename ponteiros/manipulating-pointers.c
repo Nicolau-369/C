ce8cb2d6243a5e8f29e01697fe4af54da0abed17
Nick <160781135+Nicolau-369@users.noreply.github.com>
2025-01-16 00:28:55 -0300

    Update and rename manipulando-ponteiros.c to manipulating-pointers.c

---
+++ ponteiros/manipulating-pointers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    //Declares an int variable containing the value 10
+    int count = 10;
+    //Declares a pointer to int
+    int *p;
+    //Assigns the address of the int variable to the pointer
+    p = &count;
+    
+    system("pause");
+    return 0;
+}
