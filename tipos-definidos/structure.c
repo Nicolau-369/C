ad3bfe7b2e869fc6fa1331091d5494eab794b317
Nick <160781135+Nicolau-369@users.noreply.github.com>
2025-01-16 16:47:38 -0300

    Update and rename estrutura.c to structure.c

---
+++ tipos-definidos/structure.c
@@ -0,0 +1,22 @@
+// register
+struct register{
+    char name[50];
+    int age;
+    char adress[50];
+    int number;
+};
+
+// register 2
+struct register{
+    char name[50];
+    int age;
+    char adress[50];
+    int number;
+};
+
+// register 3
+struct student{
+    char name[50];
+    int registration;
+    float note1,note2,note3;
+};
