eb9204207a4bbeb47efdf520003b84b4eb57b99a
Nick <160781135+Nicolau-369@users.noreply.github.com>
2025-01-15 21:36:15 -0300

    Update and rename comando-if-2.c to command-if-2.c

---
+++ controle-condicional/command-if-2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    int num;
+    printf ("Enter a number: ");
+    scanf("%d",&num);
+    if(num > 10); "WRONG"!
+    printf ("The number is greater than 10\n”);
+}
+system("pause");
+return 0;
