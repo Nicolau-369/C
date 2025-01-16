cd139d1cd8c1d0c84549cd037088aa284c643c14
Nick <160781135+Nicolau-369@users.noreply.github.com>
2025-01-15 23:10:08 -0300

    Update main-2.c

--- variaveis/main-2.c
+++ variaveis/main-2.c
@@ -4,17 +4,17 @@ int main(){
     int x = 65;
     char ch;
     float f = 25.1;
-    //ch recebe 8 bits menos significativos de x
-    //converte para a tabela ASCII
+    //ch receives 8 least significant bits of x
+    //convert to ASCII table
     ch = x;
     printf("ch = %c\n",ch);
-    //x recebe parte apenas a parte inteira de f
+    //x receives part only the whole part of f
     x = f;
     printf("x = %d\n",x);
-    //f recebe valor 8 bits convertido para real
+    //f receives 8-bit value converted to real
     f = ch;
     printf("f = %f\n",f);
-    //f recebe o valor de x
+    //f receives the value of x
     f = x;
     printf("f = %f\n",f);
     system("pause");
