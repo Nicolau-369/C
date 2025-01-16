a77970eec38279a4e75b3760a2cd76cb505446e7
Nick <160781135+Nicolau-369@users.noreply.github.com>
2025-01-16 12:00:50 -0300

    Update and rename passagem.c to ticket.c

--- funcoes/passagem.c
+++
@@ -1,9 +0,0 @@
-// Por valor
-void soma _ mais _ um(int n){
-    n = n + 1;
-}
-
-// Por referência
-void soma _ mais _ um(int *n){
-    *n = *n + 1;
-}
---
+++ funcoes/ticket.c
@@ -0,0 +1,9 @@
+// By value
+void sum _ more _ one(int n){
+    n = n + 1;
+}
+
+// By reference
+void sum _ more _ one(int *n){
+    *n = *n + 1;
+}
