4cf24f62307c890a14e226a27951bb7e82d84d05
Nick <160781135+Nicolau-369@users.noreply.github.com>
2025-01-16 11:09:19 -0300

    Update and rename array-como-parametro.c to array-as-parameter.c

--- funcoes/array-as-parameter.c
+++ funcoes/array-as-parameter.c
@@ -3,12 +3,12 @@
 void imprime (int *n, int m){
     int i;
     for (i=0; i<m;i++)
-        printf(“%d \n”, n[i]);
+        printf("%d \n", n[i]);
 }
 
 int main(){
     int v[5] = {1,2,3,4,5};
     imprime(v,5);
-    system(“pause”);
+    system("pause");
     return 0;
 }
