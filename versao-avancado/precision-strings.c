0d73ffdaefd6ecd8f58171226864d4f60a7ad810
Nick <160781135+Nicolau-369@users.noreply.github.com>
2025-01-16 14:07:58 -0300

    Update and rename precisao-strings.c to precision-strings.c

---
+++ versao-avancado/precision-strings.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    char text[20] = "My program C";
+    printf("%s\n",text);
+    printf("%.3s\n",text);
+    printf("%.12s\n",text);
+    system("pause");
+    return 0;
+}
