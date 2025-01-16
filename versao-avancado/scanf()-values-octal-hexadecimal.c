ebb842dfc46a9103ca05be774555f79a4b15db60
Nick <160781135+Nicolau-369@users.noreply.github.com>
2025-01-16 14:55:38 -0300

    Update and rename scanf()-valores-octal-hexadecimal.c to scanf()-values-octal-hexadecimal.c

---
+++ versao-avancado/scanf()-values-octal-hexadecimal.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    int n;
+    //reading in format octal
+    scanf("%o",&n);
+    //reading in format hexadecimal
+    scanf("%x",&n);
+    scanf("%X",&n);
+    system("pause");
+    return 0;
+}
