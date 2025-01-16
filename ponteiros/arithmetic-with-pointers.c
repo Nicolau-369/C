939abd515da4b6ee86f623420c76d3885abec269
Nick <160781135+Nicolau-369@users.noreply.github.com>
2025-01-16 00:17:28 -0300

    Update arithmetic-with-pointers.c

--- ponteiros/arithmetic-with-pointers.c
+++ ponteiros/arithmetic-with-pointers.c
@@ -2,16 +2,16 @@
 #include <stdlib.h>
 int main(){
     int *p = 0x5DC;
-    printf(“p = Hexadecimal: %p Decimal: %d \n”,p,p);
+    printf("p = Hexadecimal: %p Decimal: %d \n",p,p);
     //Increment p by one position
     p++;
-    printf(“p = Hexadecimal: %p Decimal: %d \n”,p,p);
+    printf("p = Hexadecimal: %p Decimal: %d \n",p,p);
     //Increment p by 15 positions
     p = p + 15;
-    printf(“p = Hexadecimal: %p Decimal: %d \n”,p,p);
+    printf("p = Hexadecimal: %p Decimal: %d \n",p,p);
     //Decrease p by 2 positions
     p = p - 2;
-    printf(“p = Hexadecimal: %p Decimal: %d \n”,p,p);
-    system(“pause”);
+    printf("p = Hexadecimal: %p Decimal: %d \n",p,p);
+    system("pause");
     return 0;
 }
