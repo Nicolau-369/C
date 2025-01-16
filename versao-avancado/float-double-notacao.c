92acc3c8cce1b4315df1a6dd7714c7d3e67a06dd
Nick <160781135+Nicolau-369@users.noreply.github.com>
2025-01-16 13:15:59 -0300

    Update float-double-notacao.c

--- versao-avancado/float-double-notacao.c
+++ versao-avancado/float-double-notacao.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 int main(){
     float x;
-    scanf(“%f”,&x);
-    scanf(“%e”,&x);
-    scanf(“%E”,&x);
-    scanf(“%g”,&x);
-    scanf(“%G”,&x);
-    system(“pause”);
+    scanf("%f",&x);
+    scanf("%e",&x);
+    scanf("%E",&x);
+    scanf("%g",&x);
+    scanf("%G",&x);
+    system("pause");
     return 0;
 }
