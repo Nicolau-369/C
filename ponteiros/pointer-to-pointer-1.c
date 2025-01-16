453c13ee3b15f43c32b34cae11bec8ce3c7609ef
Nick <160781135+Nicolau-369@users.noreply.github.com>
2025-01-16 00:34:23 -0300

    Update and rename ponteiro-para-ponteiro-1.c to pointer-to-pointer-1.c

---
+++ ponteiros/pointer-to-pointer-1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    //integer variable
+    int x;
+    //pointer to an integer (1 level)
+    int *p1;
+    //pointer to integer pointer (2 levels)
+    int **p2;
+    //pointer to pointer to integer pointer (3 levels)
+    int ***p3;
+    system("pause");
+    return 0;
+}
