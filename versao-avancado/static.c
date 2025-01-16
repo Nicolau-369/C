f34d295fc2951f8cf93fd175fc56230bc7fa9ec0
Nick <160781135+Nicolau-369@users.noreply.github.com>
2025-01-16 15:14:01 -0300

    Update static.c

--- versao-avancado/static.c
+++ versao-avancado/static.c
@@ -1,38 +1,38 @@
-// variáveis globais com static
+// global variables with static
 #include <stdio.h>
 #include <stdlib.h>
 int x = 20;
 static int y = 10;
 int main(){
-    printf(“x = %d\n”,x);
-    printf(“y = %d\n”,y);
-    system(“pause”);
+    printf("x = %d\n",x);
+    printf("y = %d\n",y);
+    system("pause");
     return 0;
 }
 
-// variáveis locais com static
+// local variables with static
 #include <stdio.h>
 #include <stdlib.h>
 void imprime(){
     static int n = 0;
-    printf(“%d\n”,n++);
+    printf("%d\n",n++);
 }
 int main(){
     int i;
     for(i=1; i<=10; i++)
-        imprime();
-    system(“pause”);
+        print();
+    system("pause");
     return 0;
 }
 
-// funções com static
+// functions with static
 #include <stdio.h>
 #include <stdlib.h>
-static void imprime(){
-    printf(“Executando funcao imprime()\n”);
+static void print(){
+    printf("Running print function()\n");
 }
 int main(){
-    imprime();
-    system(“pause”);
+    print();
+    system("pause");
     return 0;
 }
