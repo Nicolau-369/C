0ba2fc470efeba1bcb4692a85eacee09573e696e
Nick <160781135+Nicolau-369@users.noreply.github.com>
2025-01-15 22:12:36 -0300

    Update and rename condicao.c to condition.c

--- controle-condicional/condicao.c
+++
@@ -1,8 +0,0 @@
-x e maior ou igual a y?
-    x >= y
-    x e maior do que y+2?
-    x > y+2
-    x-5 e diferente de y+3?
-    x-5 != y+3
-    x e maior do que y e menor do que z?
-    (x > y) && (x < z)
---
+++ controle-condicional/condition.c
@@ -0,0 +1,8 @@
+x is greater than or equal to y?
+    x >= y
+    x is greater than y+2?
+    x > y+2
+    x-5 is different from y+3?
+    x-5 != y+3
+    x is greater than y and less than z?
+    (x > y) && (x < z)
