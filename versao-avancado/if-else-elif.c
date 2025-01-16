be28c7f55f45449dccdd1e2436c850775836587b
Nick <160781135+Nicolau-369@users.noreply.github.com>
2025-01-16 13:25:46 -0300

    Update if-else-elif.c

--- versao-avancado/if-else-elif.c
+++ versao-avancado/if-else-elif.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define TAMANHO 55
+#define SIZE 55
 
-#if TAMANHO > 100
-#undef TAMANHO
-#define TAMANHO 100
-#elif TAMANHO < 50
-#undef TAMANHO
-#define TAMANHO 50
+#if SIZE > 100
+#undef SIZE
+#define SIZE 100
+#elif SIZE < 50
+#undef SIZE
+#define SIZE 50
 #else
-#undef TAMANHO
-#define TAMANHO 75
+#undef SIZE
+#define SIZE 75
 #endif
 int main(){
-    printf(“Valor de TAMANHO = %d\n”,TAMANHO);
-    system(“pause”);
+    printf("Valor de SIZE = %d\n",SIZE);
+    system("pause");
     return 0;
 }
