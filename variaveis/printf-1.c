d94f7b59e5aca1fbdc4b9a7db8ca6705d982c10d
Nick <160781135+Nicolau-369@users.noreply.github.com>
2025-01-15 23:17:15 -0300

    Update printf-1.c

--- variaveis/printf-1.c
+++ variaveis/printf-1.c
@@ -3,8 +3,8 @@
 int main(){
     int x = 10;
     printf("Total = %d%f\n",x);
-    printf("%d caixas\n",x);
-    printf("Total de %d caixas\n",x);
+    printf("%d boxes\n",x);
+    printf("Total of %d boxes\n",x);
     system("pause");
     return 0;
 }
