c8d06b8f3c60b0bf5d1b1f7f378b476f999ea575
Nick <160781135+Nicolau-369@users.noreply.github.com>
2025-01-16 13:17:25 -0300

    Update and rename funcao-print().c to function-print().c

--- versao-avancado/funcao-print().c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-int main(){
-    printf(“Este texto que estou querendo escrever \
-    na tela de saida e muito grande. Por isso eu \
-    resolvi quebra-lo em varias linhas \n”);
-    system(“pause”);
-    return 0;
-}
---
+++ versao-avancado/function-print().c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    printf("This text that I want to write \
+    on the output screen and very large. That's why I \
+    I decided to break it into several lines \n");
+    system("pause");
+    return 0;
+}
