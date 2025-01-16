d5db683f246a965f3e5032fd33882be5dbc3c258
Nick <160781135+Nicolau-369@users.noreply.github.com>
2025-01-16 12:11:50 -0300

    Update and rename return-e-void.c to return-and-void.c

---
+++ funcoes/return-and-void.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+void print _ log(float x){
+    if(x <= 0)
+        return;//ends the function
+    printf("Log = %f\n",log(x));
+}
+int main(){
+    float x;
+    printf("Enter x: ");
+    scanf("%f", &f);
+    print _ log(x);
+    system("pause");
+    return 0;
+}
